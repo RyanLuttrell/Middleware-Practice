8d0545b5694877b7cf3dd9599942e4dd457159ff
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-08 08:36:02 -0700

    Completed modularization

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 const express = require('express');
-const fav = require('./fav');
+const path = require('path');
+const bodyParser = require('body-parser');
+const multiply = require('./multiply');
+
+const routes = require('./routes/index');
+
 const app = express();
-app.use(fav);
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(multiply({by: 10}));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
-    console.log('Hello')
-    const err = new Error('Holy Smokes!');
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use((req, res, next) => {
-    console.log('World');
-    next();
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
-app.use((req, res) => res.send('<h1>Express is working!</h1>'));
-app.listen(3000);
+module.exports = app;
--- fav.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.url === '/favicon.ico') {
-    return res.send('');
-  }
-  next();
-}
